5cfebff079625355298b27f88a051c304506ead8
Aakash Shah <subs@appsparkler.com>
2021-03-09 11:42:32 +0530

    Setting up TaskList

---
+++ src/components/TaskList.stories.js
@@ -0,0 +1,45 @@
+import React from "react";
+import TaskList from "./TaskList";
+import * as TaskStories from "./Task.stories";
+
+const Template = (args) => <TaskList {...args} />;
+Template.args = {};
+
+export const Default = Template.bind({});
+Default.args = {
+  tasks: [
+    { ...TaskStories.Default.args.task, id: "1", title: "Task 1" },
+    { ...TaskStories.Default.args.task, id: "2", title: "Task 2" },
+    { ...TaskStories.Default.args.task, id: "3", title: "Task 3" },
+    { ...TaskStories.Default.args.task, id: "4", title: "Task 4" },
+    { ...TaskStories.Default.args.task, id: "5", title: "Task 5" },
+    { ...TaskStories.Default.args.task, id: "6", title: "Task 6" },
+  ],
+};
+
+export const WithPinnedTasks = Template.bind({});
+WithPinnedTasks.args = {
+  tasks: [
+    ...Default.args.tasks.slice(0, 5),
+    { ...TaskStories.Pinned.args.task, id: "6", title: "Task 6 (pinned)" },
+  ],
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+  tasks: [],
+  loading: true,
+};
+
+export const Empty = Template.bind({});
+Empty.args = {
+  ...Loading.args,
+  loading: false,
+};
+
+const Story = {
+  title: "Components/Task List",
+  component: TaskList,
+};
+
+export default Story;
