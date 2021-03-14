41b962aa67cc14bb8bb72202abb67c02c3ba8bf3
Aakash Shah <aakashshah@Aakashs-MacBook-Air.local>
2021-03-14 15:58:12 +0530

    upassing key to iterated items

--- src/components/TaskInbox/TaskList/LoadingRows/index.js
+++ src/components/TaskInbox/TaskList/LoadingRows/index.js
@@ -4,7 +4,7 @@ import LoadingRow from './LoadingRow'
 const LoadingRows = () => (
     <div className="list-items">
         {[...new Array(6)].map((item, key) => <LoadingRow key={key} />)}
-      </div>
+    </div>
 )
 
 export default LoadingRows;
