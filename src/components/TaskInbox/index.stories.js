b0f21bd33eff05b40197471b03ed19c4da729878
Aakash Shah <subs@appsparkler.com>
2021-03-14 17:40:20 +0530

    updated stories for pure component

--- src/components/TaskInbox/index.stories.js
+++ src/components/TaskInbox/index.stories.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TaskInbox from "./index";
+import TaskList from "./TaskList";
 import * as TaskListStories from "./TaskList/index.stories";
 
 const Template = args => <TaskInbox {...args} />
@@ -7,8 +8,28 @@ const Template = args => <TaskInbox {...args} />
 export const Loading = Template.bind({});
 Loading.args = {
   loading: true,
-  tasks: [],
+  tasks: TaskListStories.Default.args.tasks,
   error: false
+}
+
+export const LoadedWithTasks = Template.bind({});
+LoadedWithTasks.args = {
+  ...Loading.args,
+  tasks: TaskListStories.WithAllTaskTypes.args.tasks,
+  loading: false
+}
+
+export const LoadedCompletedTasks = Template.bind({});
+LoadedCompletedTasks.args = {
+  ...Loading.args,
+  tasks: TaskListStories.WithAllTaskTypes.args.tasks,
+  loading: false
+}
+
+export const LoadingError = Template.bind({});
+LoadingError.args = {
+  ...Loading.args,
+  error: 'Server is down.  Please re-try later.'
 }
 
 const Story = {
