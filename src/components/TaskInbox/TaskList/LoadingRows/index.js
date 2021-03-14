93c7ba43789fc9ff0b72f4aad5a61ad0681aef25
Aakash Shah <aakashshah@Aakashs-MacBook-Air.local>
2021-03-14 15:35:57 +0530

    Rendering all cases with a connnected Redux store

--- src/components/TaskInbox/TaskList/LoadingRows/index.js
+++ src/components/TaskInbox/TaskList/LoadingRows/index.js
@@ -3,7 +3,7 @@ import LoadingRow from './LoadingRow'
 
 const LoadingRows = () => (
     <div className="list-items">
-        {[...new Array(6)].map(() => <LoadingRow />)}
+        {[...new Array(6)].map((item, key) => <LoadingRow key={key} />)}
       </div>
 )
 
--- src/components/TaskInbox/index.stories.js
+++ src/components/TaskInbox/index.stories.js
@@ -1,44 +1,110 @@
 import React from "react";
 import { PureInboxScreen } from "./index";
+import {createStore} from 'redux'
 import { Provider } from "react-redux";
-import { action } from "@storybook/addon-actions";
 import * as TaskListStories from "./TaskList/index.stories";
+import TaskInbox from './'
 
-const store = {
-  getState: () => {
-    return {
-      tasks: TaskListStories.WithAllTaskStates.args.tasks,
-      loading: true,
-    };
-  },
-  subscribe: () => 0,
-  dispatch: action("dispatch"),
+const PRELOADED_STATE = {
+  loading: true,
+  tasks: []
+}
+
+const actions = {
+  SET_IS_LOADING: "SET_IS_LOADING",
+  SET_TASKS: "SET_TASKS",
+  SET_ERROR: "SET_ERROR"
+}
+
+const mainReducer = (state, action) => {
+  const {type: actionType, payload} = action;
+  switch (actionType) {
+    case actions.SET_IS_LOADING:
+      return {
+        ...state,
+        loading: payload
+      }
+    case actions.SET_TASKS:
+        return {
+          ...state,
+          tasks: payload
+        }
+
+    case actions.SET_ERROR:
+      return {
+        ...state,
+        error: payload
+      }
+  
+    default:
+      return {...state}
+  }
+}
+
+const my_redux_store = createStore(mainReducer, PRELOADED_STATE)
+
+const setIsLoading = (payload = false) => my_redux_store.dispatch({
+  type: actions.SET_IS_LOADING,
+  payload
+})
+
+const setTasks = (payload = []) => my_redux_store.dispatch({
+  type: actions.SET_TASKS,
+  payload
+})
+
+const setError = (payload = []) => my_redux_store.dispatch({
+  type: actions.SET_ERROR,
+  payload
+})
+
+export const Loading = () => {
+  React.useEffect(() => {
+    setError(null)
+    setIsLoading(true);
+  }, [])
+  return <TaskInbox />
 };
+Loading.args = {
+  error: null,
+}
 
-const Template = (args) => <PureInboxScreen {...args} />;
-
-export const Default = Template.bind({});
-Default.args = {
+export const LoadedWithTasks = () => {
+  React.useEffect(() => {
+    setError(null)
+    setIsLoading(false);
+    setTasks(TaskListStories.WithAllTaskStates.args.tasks)
+  }, [])
+  return <TaskInbox />
+};
+LoadedWithTasks.args = {
   error: null,
 };
 
-export const Loading = Template.bind({});
-Default.args = {
-  error: null,
-};
+export const LoadedWithNoTasks = () => {
+  React.useEffect(() => {
+    setError(null);
+    setIsLoading(false);
+    setTasks([])
+  }, []);
+  return <TaskInbox />
+}
 
-export const Error = Template.bind({});
-Error.args = {
-  ...Default.args,
-  error: "Server is down!",
-};
+export const Error = () => {
+  React.useEffect(() => {
+    setIsLoading(false);
+    setTasks([]);
+    setError('Server is down.  Please try again in some time.')
+  }, []);
+  return <TaskInbox />
+}
 
 const Story = {
   title: "Pages/Task Inbox",
   component: PureInboxScreen,
   decorators: [
     (Story) => (
-      <Provider store={store}>
+      <Provider store={my_redux_store}>
         <Story />
       </Provider>
     ),
